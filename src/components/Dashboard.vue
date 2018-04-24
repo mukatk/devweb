6a6036a2fc0f1e083a86c53ec070ebb710631c3c
mukatk <mukatk@gmail.com>
2018-04-23 23:26:23 -0300

    Lista de despesas parcial

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -8,10 +8,26 @@
       </v-card>
       <v-spacer class="mt-2"></v-spacer>
       <v-tabs grow show-arrows slider-color="primary" v-model="mesSelecionado">
-        <v-tab v-for="mes in meses" :key="mes" ripple>{{mes.substring(0, 3)}}</v-tab>
-        <v-tab-item lazy v-for="mes in meses" :key="mes">
+        <v-tab v-for="item in meses" :key="item.mes" ripple>{{item.mes.substring(0, 3)}}</v-tab>
+        <v-tab-item lazy v-for="(item, index) in meses" :key="item.mes">
           <v-card flat>
-            <v-card-text>{{mes}}</v-card-text>
+            <v-card-text>
+              <v-btn v-if="index >= mesAtual" color="primary">Adicionar despesa</v-btn>
+
+              <v-list>
+                <v-list-tile v-for="despesa in item.despesas" :key="despesa.id">
+                  <v-list-tile-action>
+                    <v-icon color="primary">food</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                    <v-list-tile-title v-text="despesa.descricao"></v-list-tile-title>
+                  </v-list-tile-content>
+                  <v-list-tile-avatar>
+                    <v-icon></v-icon>
+                  </v-list-tile-avatar>
+                </v-list-tile>
+              </v-list>
+            </v-card-text>
           </v-card>
         </v-tab-item>
       </v-tabs>
@@ -23,17 +39,42 @@
 export default {
   name: 'Dashboard',
   mounted: function () {
-    this.mesSelecionado = (new Date().getMonth()).toString();
+    this.mesSelecionado = this.toString();
   },
   computed: {
+    mesAtual: function () {
+      return (new Date().getMonth());
+    },
     usuariologado: function () {
         return JSON.parse(sessionStorage.getItem('usuariologado'));
+    }
+  },
+  watch: {
+    mesSelecionado: function (val) {
+      if (val == 1) {
+        this.meses[val].despesas.push({
+          descricao: 'Comida'
+        });
+      }
     }
   },
   data: () => {
     return {
       mesSelecionado: '0',
-      meses: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+      meses: [
+        { mes: "Janeiro", despesas: [] },
+        { mes: "Fevereiro", despesas: [] },
+        { mes: "Março", despesas: [] },
+        { mes: "Abril", despesas: [] },
+        { mes: "Maio", despesas: [] },
+        { mes: "Junho", despesas: [] },
+        { mes: "Julho", despesas: [] },
+        { mes: "Agosto", despesas: [] },
+        { mes: "Setembro", despesas: [] },
+        { mes: "Outubro", despesas: [] },
+        { mes: "Novembro", despesas: [] },
+        { mes: "Dezembro", despesas: [] },
+      ]
     }
   },
   methods: {
